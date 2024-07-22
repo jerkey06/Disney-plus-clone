1a3daf4f7cee4cdf0a1935c87b63ee42c5793ac6
jerkey06 <ponceemiliano44@gmail.com>
2024-07-22 16:40:12 -0600

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import logo from '../assets/images/logo.svg';
+import { HiHome, HiMagnifyingGlass, HiStar, HiPlayCircle, HiTv  } from "react-icons/hi2";
+import { HiPlus, HiDotsvertical } from "react-icons/hi";
+
 
 function Header() {
+    const menuItems = [
+        { name: 'Home', icon: <HiHome /> },
+        { name: 'Search', icon: <HiMagnifyingGlass /> },
+        { name: 'Watchlist', icon: <HiStar /> },
+        { name: 'Originals', icon: <HiPlayCircle /> },
+        { name: 'Movies', icon: <HiTv /> },
+    ];
     return (
         <img src="{logo}" className='w-[80px] md:w-[115px] object-cover' />
     );
